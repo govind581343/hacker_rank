f91baadb3f8bdad052bd63234459d06e5f6b6dbe
govind581343 <124591309+govind581343@users.noreply.github.com>
2024-06-10 15:50:02 +0530

    Create program_to_get_a_character_as_an_input_and_print_its_ASCII_value.c

---
+++ program_to_get_a_character_as_an_input_and_print_its_ASCII_value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char ch;
+    scanf("%c", &ch);
+
+    printf("%d\n", (int)ch);
+
+    return 0;
+}
